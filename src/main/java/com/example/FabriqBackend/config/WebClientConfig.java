5a08d1daebb6448ec038a2ca0a35f6eb97084b93
medhani premathilaka <mpremathilaka1@gmail.com>
2025-12-03 13:48:06 +0530

    feat: update CORS configuration and fix package declaration in WebClientConfig

--- src/main/java/com/example/FabriqBackend/config/WebClientConfig.java
+++ src/main/java/com/example/FabriqBackend/config/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.yourapp.config;
+package com.example.FabriqBackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
