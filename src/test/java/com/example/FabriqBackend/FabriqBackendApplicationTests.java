c78835d1aebaeee0db65ea79a45f017f2944134c
medhani premathilaka <mpremathilaka1@gmail.com>
2025-12-10 15:52:34 +0530

    feat: exclude Redis auto-configuration in test profile for improved test isolation

--- src/test/java/com/example/FabriqBackend/FabriqBackendApplicationTests.java
+++ src/test/java/com/example/FabriqBackend/FabriqBackendApplicationTests.java
@@ -1,19 +1,23 @@
 package com.example.FabriqBackend;
 
+import com.example.FabriqBackend.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestConfig.class)
 @EnableAutoConfiguration(exclude = {
 		RedisAutoConfiguration.class,
 		RedisRepositoriesAutoConfiguration.class
 })
 class FabriqBackendApplicationTests {
+
 
 	@Test
 	void contextLoads() {
