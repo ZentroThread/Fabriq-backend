724f9151a65cb01427fca1373563bede5fdeda80
medhani premathilaka <mpremathilaka1@gmail.com>
2026-01-18 19:03:13 +0530

    refactor: removed unnecessary measurement files

--- src/main/java/com/example/FabriqBackend/dao/MeasurementDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.FabriqBackend.dao;
-
-import com.example.FabriqBackend.model.Measurement;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MeasurementDao extends TenantAwareDao<Measurement, Integer> {
-}
