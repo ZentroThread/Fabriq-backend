cfb178c9f67815d422a605dbff3545d315e225b7
medhani premathilaka <mpremathilaka1@gmail.com>
2026-01-22 21:39:54 +0530

    feat: update CORS configuration to allow requests from Vercel frontend

--- src/main/java/com/example/FabriqBackend/config/CorsConfig.java
+++ src/main/java/com/example/FabriqBackend/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174", "http://localhost:5173", "http://localhost:5175")
+                        .allowedOrigins("http://localhost:5174"
+                                , "http://localhost:5173"
+                                , "http://localhost:5175"
+                                , "https://fabriq-frontend.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization", "X-Tenant-ID")
